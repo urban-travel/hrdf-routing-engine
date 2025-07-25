8ab56e0386743bfd3b527ed85ad6e5e32fa5ef3d
Orestis <orestis.malaspinas@pm.me>
2025-07-26 00:24:02 +0300

    corrected old_departure_at option

--- src/main.rs
+++ src/main.rs
@@ -86,9 +86,9 @@ enum Mode {
     Compare {
         #[command(flatten)]
         isochrone_args: IsochroneArgsBuilder,
-        /// Departure date and time
+        /// Second departure date and time
         #[arg(short, long, default_value_t = String::from("2024-04-11 15:36:00"))]
-        departure_at_old: String,
+        old_departure_at: String,
         /// Display mode of the isochrones: circles or contour_line
         #[arg(long, default_value_t = IsochroneDisplayMode::Circles)]
         mode: IsochroneDisplayMode,
@@ -238,12 +238,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Mode::Compare {
             isochrone_args,
             mode,
-            departure_at_old,
+            old_departure_at,
             delta_time,
         } => {
             let args_2025 = isochrone_args.clone().finalize()?;
             let args_2024 = isochrone_args
-                .set_departure_at(departure_at_old)
+                .set_departure_at(old_departure_at)
                 .finalize()?;
 
             let hrdf_2024 = Hrdf::new(
